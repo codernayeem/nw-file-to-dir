6f32492edce12c29f944269f43cea8af239a80b4
Md Nayeem <codernayeem898@gmail.com>
2020-06-14 21:14:25 +0600

    Added main Window

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,21 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5 import QtGui, QtWidgets, QtCore
+from main_ui import Ui_MainWindow
 
-import sys
+import sys, os
+
+app = ApplicationContext()
+
+class MainWindow(QtWidgets.QMainWindow):
+    ui = Ui_MainWindow()
+    app_version = app.build_settings['version']
+
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setWindowTitle("File to Dir")
+        self.ui.setupUi(self)
 
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
-    window.resize(250, 150)
-    window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
-    sys.exit(exit_code)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.app.exec_())
