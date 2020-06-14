40a3b6e593c4bd834e96c1611a3e2b592c4693f7
Md Nayeem <codernayeem898@gmail.com>
2020-06-15 01:00:41 +0600

    Added main function (Copy / Move)

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,8 +1,9 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5 import QtGui, QtWidgets, QtCore
 from main_ui import Ui_MainWindow
-from tools import FileData, join, Path, is_valid_dir
 from dialogs import Ui_AboutPage
+from tools import FileData, is_valid_dir, join, Path, get_filename_extension, get_files
+from shutil import copyfile, move
 
 import sys, os
 
@@ -75,7 +76,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # button
         self.ui.output_bt.clicked.connect(self.select_output_folder)
-        # self.ui.bt_go.clicked.connect(self.go_for_work)
+        self.ui.bt_go.clicked.connect(self.go_for_work)
 
     def select_folder(self, multiple=False, drag=False, folders=None):
         if multiple:
@@ -142,6 +143,111 @@ class MainWindow(QtWidgets.QMainWindow):
             self.about_page.ui.name.setText("File To Dir")
         self.about_page.destroy()
         self.about_page.show()
+
+    def go_for_work(self):
+        i1 = self.ui.input_1.currentIndex()
+        i2 = self.ui.input_2.text()
+        include_subfolder = self.ui.side_input_1.isChecked()
+        if i2.strip() == '':
+            return QtWidgets.QMessageBox.warning(self, "Warning", f"<p style=\"font-size: 10pt;\"><b>Error Found.</b> Please, give Output Directory.</p>")
+        i2 = Path(i2)
+        if not (i2.exists() and i2.is_dir()):
+            return QtWidgets.QMessageBox.warning(self, "Warning", f"<p style=\"font-size: 10pt;\"><b>Error Found.</b> Output Directory not exist.</p>")
+
+        folders = self.FILEDATA.get_all_folders(include_subfolder)
+
+        data = {
+            's2': self.ui.side_input_2.isChecked(),
+            's3': self.ui.side_input_3.isChecked(),
+            's4': self.ui.side_input_4.text(),
+            's5': self.ui.side_input_5.text(),
+            's6': self.ui.side_input_6.text(),
+            's7': self.ui.side_input_7.text(),
+            's8': self.ui.side_input_8.isChecked(),
+            's9': self.ui.side_input_9.isChecked(),
+            's10': self.ui.side_input_10.text(),
+            's11': self.ui.side_input_11.text(),
+            's12': self.ui.side_input_12.text(),
+            's13': self.ui.side_input_13.text(),
+        }
+
+        done = 0
+        fail = 0
+        ignored = 0
+        not_allowed = 0
+
+        for folder in folders:
+            for a_file in get_files(folder):
+                status = self.check_for_allow_and_ignore(a_file, data)
+                if status == 1:
+                    ignored += 1
+                elif status == 2:
+                    not_allowed += 1
+                else:
+                    try:
+                        if i1 == 0:
+                            copyfile(join(folder, a_file), join(i2, a_file))
+                        else:
+                            move(join(folder, a_file), join(i2, a_file))
+                        done += 1
+                    except:
+                        fail += 1
+
+        info = f"<p style=\"font-size: 10pt;\"><b>Finished.</b> {done} files "
+        if i1 == 0:
+            info += 'copied'
+        else:
+            info += 'moved'
+        if fail:
+            info += f" | {fail} files failed"
+        if ignored:
+            info += f" | {ignored} files ignored"
+        if not_allowed:
+            info += f" | {not_allowed} files not allowed"
+        info += '.'
+        QtWidgets.QMessageBox.warning(self, "Info", info)
+
+    def check_for_allow_and_ignore(self, fl_name, data):
+        filename, ext = get_filename_extension(fl_name)
+        if data['s8']:
+            if (data['s9'] and not ext) or (data['s10'] and ext in data['s10']):
+                return 1
+            if data['s11']:
+                for i in data['s11']:
+                    if filename.startswith(i):
+                        return 1
+            if data['s12']:
+                for i in data['s12']:
+                    if filename.endswith(i):
+                        return 1
+            if data['s13']:
+                for i in data['s13']:
+                    if i in filename:
+                        return 1
+
+        if data['s2']:
+            add_item = False
+            if (data['s3'] and not ext) or (data['s4'] and ext in data['s4']):
+                add_item = True
+            elif data['s5']:
+                for i in data['s5']:
+                    if filename.startswith(i):
+                        add_item = True
+                        break
+            elif data['s6']:
+                for i in data['s6']:
+                    if filename.endswith(i):
+                        add_item = True
+                        break
+            elif data['s7']:
+                for i in data['s7']:
+                    if i in filename:
+                        add_item = True
+                        break
+            if not add_item:
+                return 2
+        return 0
+
 
 if __name__ == '__main__':
     mainWindow = MainWindow()
