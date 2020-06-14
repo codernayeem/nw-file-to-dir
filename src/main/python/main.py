df33b4015b4ea48f7cbb8e6c366042eee014b2ce
Md Nayeem <codernayeem898@gmail.com>
2020-06-15 00:31:35 +0600

    Added Folder Selection System

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,6 +1,7 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5 import QtGui, QtWidgets, QtCore
 from main_ui import Ui_MainWindow
+from tools import FileData, join, Path, is_valid_dir
 
 import sys, os
 
@@ -8,9 +9,11 @@ app = ApplicationContext()
 
 class MainWindow(QtWidgets.QMainWindow):
     ui = Ui_MainWindow()
+    FILEDATA = FileData()
 
     app_version = app.build_settings['version']
     main_icon = app.get_resource('icon.png')
+    last_selected_dir = 'C:'
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -35,25 +38,53 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.actionAdd_Multiple_Folder.setIcon(QtGui.QIcon(app.get_resource('add.png')))
         self.ui.actionAbout_the_Author.setIcon(QtGui.QIcon(self.main_icon))
 
-
     def set_menu_toolbar_button_action(self):
         # menu
         # self.ui.actionAbout_the_Author.triggered.connect(self.see_about)
         self.ui.actionExit_1.triggered.connect(lambda: sys.exit(0))
-        # self.ui.actionAdd_1.triggered.connect(self.select_folder)
-        # self.ui.actionAdd_Multiple_Folder.triggered.connect(lambda: self.select_folder(multiple=True))
-        # self.ui.actionClear.triggered.connect(self.clear_all_folders)
+        self.ui.actionAdd_1.triggered.connect(self.select_folder)
+        self.ui.actionAdd_Multiple_Folder.triggered.connect(lambda: self.select_folder(multiple=True))
+        self.ui.actionClear.triggered.connect(self.clear_all_folders)
         # self.ui.actionReset.triggered.connect(self.reset_all)
 
         # toolbar
-        # self.ui.actionAdd.triggered.connect(self.select_folder)
-        # self.ui.actionClear.triggered.connect(self.clear_all_folders)
+        self.ui.actionAdd.triggered.connect(self.select_folder)
+        self.ui.actionClear.triggered.connect(self.clear_all_folders)
         # self.ui.actionReset.triggered.connect(self.reset_all)
         self.ui.actionExit.triggered.connect(sys.exit)
 
         # button
         # self.ui.output_bt.clicked.connect(self.select_output_folder)
         # self.ui.bt_go.clicked.connect(self.go_for_work)
+
+    def select_folder(self, multiple=False, drag=False, folders=None):
+        if multiple:
+            file_dialog = QtWidgets.QFileDialog(directory=self.last_selected_dir)
+            file_dialog.setFileMode(QtWidgets.QFileDialog.DirectoryOnly)
+            file_dialog.setOption(QtWidgets.QFileDialog.DontUseNativeDialog, True)
+            file_dialog.setWindowTitle("Select Folders")
+            file_dialog.setWindowIcon(QtGui.QIcon(self.main_icon))
+            f_tree_view = file_dialog.findChild(QtWidgets.QTreeView)
+            if f_tree_view:
+                f_tree_view.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+            if not file_dialog.exec():
+                return
+            folders = file_dialog.selectedFiles()
+        elif drag:
+            folders = [f for f in folders if is_valid_dir(f)]
+        else:
+            folder = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory", directory=self.last_selected_dir))
+            if folder == '':
+                return
+            folder = str(Path(folder))
+            self.last_selected_dir = join(*folder.split('\\')[:len(folder.split('\\'))-1])
+            folders = [folder]
+        self.FILEDATA.select_dirs(folders)
+        self.ui.txt_stat.setText(self.FILEDATA.get_status_txt())
+
+    def clear_all_folders(self):
+        self.FILEDATA.reset()
+        self.ui.txt_stat.setText(self.FILEDATA.get_status_txt())
 
 
 if __name__ == '__main__':
---
+++ src/main/python/tools.py
@@ -0,0 +1,123 @@
+from pathlib import Path
+from os.path import join, isfile, isdir, getsize, getctime, getmtime
+from os import listdir, rename
+import time, json
+from datetime import datetime
+
+def is_valid_file(pth):
+    try:
+        p = Path(pth)
+        if p.exists() and p.is_file():
+            return True
+    except:
+        pass
+    return False
+
+def is_valid_dir(pth):
+    try:
+        p = Path(pth)
+        if p.exists() and p.is_dir():
+            return True
+    except:
+        pass
+    return False
+
+def get_splitted_by_pipe(s):
+    l = []
+    splited = str(s).split('|')
+    for i in splited:
+        if i != '':
+            l.append(i)
+    return l
+
+def get_files(pth):
+    try:
+        pth = Path(pth)
+        if pth.exists() and pth.is_dir():
+            return [str(f) for f in listdir(pth) if isfile(join(pth, f))]
+    except Exception as e:
+        print('Error on getting files : ', e)
+    return []
+
+def get_folders(pth):
+    try:
+        pth = Path(pth)
+        if pth.exists() and pth.is_dir():
+            return [join(pth, f) for f in listdir(pth) if isdir(join(pth, f))]
+    except Exception as e:
+        print('Error on getting folders : ', e)
+    return []
+
+def get_subfolders(pth):
+    all_folder = []
+    folders = get_folders(pth)
+    all_folder += folders
+    for i in folders:
+        all_folder += get_subfolders(i)
+    return all_folder
+
+def check_plural(c):
+    if c > 1:
+        return 's'
+    return ''
+
+def is_valid_filename(s):
+    if s is not None:
+        s = str(s)
+        for i in ['/', '\\', '?', '"', '|', '<', '>', ':', '*']:
+            if i in s:
+                return False
+        return True
+    return False
+
+def get_filename_extension(filename):
+    if '.' not in filename:
+        return filename, ''
+    ext = filename.split('.')[-1]
+    if '.' + ext == filename:
+        return filename, ''
+    return filename[:-(len('.'+ext))], ext
+
+def get_all_files(folders):
+    l = []
+    for i in folders:
+        l += get_files(i)
+    return l
+
+def get_int(i):
+    for a in str(i):
+        if a not in ['-','1','2','3','4','5','6','7','8','9','0']:
+            return None
+    try:
+        return int(i)
+    except:
+        return None
+
+class FileData:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.selected_folders = []
+
+    def get_all_folders(self, include_subfolder):
+        if not include_subfolder:
+            return self.selected_folders
+        a = []
+        for i in self.selected_folders:
+            a.append(i)
+            a += get_subfolders(i)
+        return a
+
+    def select_dirs(self, folders):
+        for folder in folders:
+            folder = str(Path(folder))
+            if folder not in self.selected_folders:
+                self.selected_folders.append(folder)
+
+    def get_status_txt(self):
+        c = len(self.selected_folders)
+        if c == 0:
+            return "<big>No <b>Folder</b> is selected</big>"
+        else:
+            return f"<b>{c} Folder{check_plural(c)}</b> are selected"
